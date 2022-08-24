1935c27f44a4a33fa2614386813ed0eb18edc8bd
Billy Anderson Matjik <billy.matjik@anteraja.id>
2022-08-24 12:21:01 +0700

    Deprecated UsbPluginService.kt

--- usbdetectlib/src/main/java/com/bmatjik/usbdetectlib/service/UsbPluginService.kt
+++ usbdetectlib/src/main/java/com/bmatjik/usbdetectlib/service/UsbPluginService.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
+@Deprecated("Heavy Load")
 class UsbPluginService : Service(), CoroutineScope {
     private var coroutineJob: Job = Job()
     override val coroutineContext: CoroutineContext
